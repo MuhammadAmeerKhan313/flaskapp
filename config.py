a7672dd6f2f7c1d793f98c0686d1a85e8cafe13f
MuhammadAmeerKhan313 <313ameerkhan@gmail.com>
2025-12-11 03:47:19 +0500

    Change database name from EduPredictDB to EduPredictCluster
    Updated database name in get_db function.

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
-# config.py
 from pymongo import MongoClient
 
 def get_db():
-    # Directly using your MongoDB Atlas connection URI
     uri = "mongodb+srv://Muhammad_Ameer_Khan:Ameer123%40456@edupredictcluster.txdhy5t.mongodb.net/EduPredictDB?retryWrites=true&w=majority"
-    
-    # Connect to MongoDB
     client = MongoClient(uri)
-    
-    # Select your database
-    db = client.get_database("EduPredictDB")
+    db = client.get_database("EduPredictCluster")
     return db
