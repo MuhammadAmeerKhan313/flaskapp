7c45d2477b65b3a27a88a5f0688e735e809c8297
MuhammadAmeerKhan313 <313ameerkhan@gmail.com>
2025-12-11 03:41:09 +0500

    Modify MongoDB connection in config.py
    Updated MongoDB connection URI for EduPredictDB.

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
+# config.py
 from pymongo import MongoClient
-import os
 
 def get_db():
+    # Directly using your MongoDB Atlas connection URI
     uri = "mongodb+srv://Muhammad_Ameer_Khan:Ameer123%40456@edupredictcluster.txdhy5t.mongodb.net/EduPredictDB?retryWrites=true&w=majority"
+    
+    # Connect to MongoDB
     client = MongoClient(uri)
+    
+    # Select your database
     db = client.get_database("EduPredictDB")
     return db
